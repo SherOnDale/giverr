7febf57f041d3227f0a8ef30c7eac432f757d147
Sherin Binu <sherinbinu@hotmail.com>
2019-03-09 16:04:17 +0530

    add response class

---
+++ api/utils/classes/Response.js
@@ -0,0 +1,22 @@
+class Response {
+  constructor() {
+    this.error = true;
+    this.message =
+      'There was a problem handling your request. Please try again later.';
+    this.payload = {};
+  }
+
+  setSuccess() {
+    this.error = true;
+  }
+
+  setMessage(message) {
+    this.message = message;
+  }
+
+  setPayload({ key, value }) {
+    this.payload[key] = value;
+  }
+}
+
+module.exports = Response;
